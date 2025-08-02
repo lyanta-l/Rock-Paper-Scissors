1d0f6114890c99a14a4510f239a8b86fce054f5e
lyanta-l <lyanta.why@gmail.com>
2025-08-02 18:16:30 +0800

    feat:Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hello");
+function getRandomIntInclusive(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
+function getComputerChoice() {
+    let i=getRandomIntInclusive(0,2);
+    switch(i) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+}
